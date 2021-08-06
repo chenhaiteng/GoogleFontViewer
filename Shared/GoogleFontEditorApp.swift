7543b15786eff5e181c4ba82ecf1e0e6904b1f7a
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-08-06 09:57:37 +0800

    Initial Commit

---
+++ Shared/GoogleFontEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleFontEditorApp.swift
+//  Shared
+//
+//  Created by Chen Hai Teng on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GoogleFontEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
